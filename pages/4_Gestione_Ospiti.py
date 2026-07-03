bff2b6f4b8ceb6d23c8f90816d212543f4a06e07
danielefrancia <danielefrancia@gmail.com>
2026-07-03 15:50:45 +0200

    Refactor guest modification logic in Gestione Ospiti
    Refactored guest selection and update process to streamline code and improve readability.

--- pages/4_Gestione_Ospiti.py
+++ pages/4_Gestione_Ospiti.py
@@ -15,47 +15,33 @@ df = df.dropna(subset=['ID'])
 id_selezionato = st.selectbox("Seleziona ID ospite da modificare", df['ID'].unique())
 
 if id_selezionato:
-    # Filtriamo i dati
-    mask = df['ID'] == id_selezionato
-    df_filtrato = df[mask]
+    # Filtriamo i dati dell'ospite scelto
+    df_filtrato = df[df['ID'] == id_selezionato]
     
-    # CONTROLLO DI SICUREZZA: verifichiamo che il filtro abbia trovato qualcosa
     if not df_filtrato.empty:
         ospite_data = df_filtrato.iloc[0]
         
         with st.form("form_modifica"):
             st.subheader(f"Modifica dati di {ospite_data['Nome']} {ospite_data['Cognome']}")
             
-            # ... resto del form uguale a prima ...
+            # Campi pre-compilati con i dati attuali
+            nuovo_nome = st.text_input("Nome", value=str(ospite_data['Nome']))
+            nuovo_cognome = st.text_input("Cognome", value=str(ospite_data['Cognome']))
+            nuova_camera = st.text_input("Camera", value=str(ospite_data['Camera']))
+            nuovo_tel = st.text_input("Tel familiare", value=str(ospite_data['Tel familiare']))
+            
+            btn_salva = st.form_submit_button("Salva modifiche")
 
-# 1. Selezione ospite tramite menu
-id_selezionato = st.selectbox("Seleziona ID ospite da modificare", df['ID'].unique())
-
-if id_selezionato:
-    # Filtriamo i dati dell'ospite scelto
-    ospite_data = df[df['ID'] == id_selezionato].iloc[0]
-    
-    with st.form("form_modifica"):
-        st.subheader(f"Modifica dati di {ospite_data['Nome']} {ospite_data['Cognome']}")
-        
-        # Campi pre-compilati con i dati attuali
-        nuovo_nome = st.text_input("Nome", value=ospite_data['Nome'])
-        nuovo_cognome = st.text_input("Cognome", value=ospite_data['Cognome'])
-        nuova_camera = st.text_input("Camera", value=ospite_data['Camera'])
-        nuovo_tel = st.text_input("Tel familiare", value=ospite_data['Tel familiare'])
-        
-        btn_salva = st.form_submit_button("Salva modifiche")
-
-    if btn_salva:
-        # Aggiorniamo il DataFrame
-        df.loc[df['ID'] == id_selezionato, ['Nome', 'Cognome', 'Camera', 'Tel familiare']] = [
-            nuovo_nome, nuovo_cognome, nuova_camera, nuovo_tel
-        ]
-        
-        # Salviamo su Google Sheets
-        conn.update(worksheet="Ospiti", data=df)
-        st.success("Modifiche salvate con successo!")
-        st.rerun()
+        if btn_salva:
+            # Aggiorniamo il DataFrame locale
+            df.loc[df['ID'] == id_selezionato, ['Nome', 'Cognome', 'Camera', 'Tel familiare']] = [
+                nuovo_nome, nuovo_cognome, nuova_camera, nuovo_tel
+            ]
+            
+            # Salviamo su Google Sheets sovrascrivendo il foglio
+            conn.update(worksheet="Ospiti", data=df)
+            st.success("Modifiche salvate con successo!")
+            st.rerun()
 
 # Visualizzazione tabella
 st.divider()
