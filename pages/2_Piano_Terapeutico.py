793aa22930f2a30ccc4d7d3ff9b811024804e4d9
danielefrancia <danielefrancia@gmail.com>
2026-07-03 16:10:47 +0200

    Handle empty guest list and ensure ID is string
    Added error handling for empty guest list and ensured ID is a string.

--- pages/2_Piano_Terapeutico.py
+++ pages/2_Piano_Terapeutico.py
@@ -5,15 +5,30 @@ from streamlit_gsheets import GSheetsConnection
 st.title("💊 Inserimento Piano Terapeutico")
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Leggi ospiti
+# Leggi ospiti e gestisci il caso in cui il foglio sia vuoto
 df_ospiti = conn.read(worksheet="Ospiti", ttl=0)
-# Assicuriamoci che l'ID sia stringa per evitare errori
+
+if df_ospiti.empty:
+    st.error("Nessun ospite trovato nel database. Inserisci prima un ospite nella pagina dedicata.")
+    st.stop() # Ferma l'esecuzione della pagina
+
+# Assicuriamoci che l'ID sia stringa
 df_ospiti['ID'] = df_ospiti['ID'].astype(str)
 
-lista_nomi = df_ospiti['Nome'] + " " + df_ospiti['Cognome'] + " (ID: " + df_ospiti['ID'] + ")"
+# Creazione lista con controllo sui dati mancanti
+lista_nomi = (df_ospiti['Nome'].fillna('') + " " + 
+              df_ospiti['Cognome'].fillna('') + " (ID: " + 
+              df_ospiti['ID'] + ")")
+
+ospite_selezionato = st.selectbox("Seleziona Ospite", lista_nomi)
+
+# Eseguiamo lo split solo se ospite_selezionato non è None
+if ospite_selezionato and "(ID: " in ospite_selezionato:
+    id_ospite = ospite_selezionato.split("(ID: ")[1].replace(")", "")
+else:
+    id_ospite = None
 
 with st.form("form_terapia", clear_on_submit=True):
-    ospite_selezionato = st.selectbox("Seleziona Ospite", lista_nomi)
     farmaco = st.text_input("Nome Farmaco")
     dosaggio = st.text_input("Dosaggio")
     orario = st.time_input("Orario Somministrazione")
@@ -21,25 +36,20 @@ with st.form("form_terapia", clear_on_submit=True):
     submitted = st.form_submit_button("Salva Terapia")
 
 if submitted:
-    # Estraiamo l'ID correttamente
-    id_ospite = ospite_selezionato.split("(ID: ")[1].replace(")", "")
-    
-    # Prepariamo la riga con i nomi identici al foglio pulito
-    nuova_terapia = pd.DataFrame([{
-        "ID Ospite": id_ospite,
-        "Farmaco": farmaco,
-        "Dosaggio": dosaggio,
-        "Orario": str(orario),
-        "Stato": "Da fare",
-        "Note": note_terapia
-    }])
-    
-    # Leggi terapie attuali
-    df_terapie = conn.read(worksheet="Terapie", ttl=0)
-    
-    # Unisci
-    aggiornato = pd.concat([df_terapie, nuova_terapia], ignore_index=True)
-    
-    # Salva
-    conn.update(worksheet="Terapie", data=aggiornato)
-    st.success("Terapia salvata!")
+    if not id_ospite:
+        st.error("Seleziona un ospite valido!")
+    else:
+        # Il resto del codice di salvataggio rimane uguale
+        nuova_terapia = pd.DataFrame([{
+            "ID Ospite": id_ospite,
+            "Farmaco": farmaco,
+            "Dosaggio": dosaggio,
+            "Orario": str(orario),
+            "Stato": "Da fare",
+            "Note": note_terapia
+        }])
+        
+        df_terapie = conn.read(worksheet="Terapie", ttl=0)
+        aggiornato = pd.concat([df_terapie, nuova_terapia], ignore_index=True)
+        conn.update(worksheet="Terapie", data=aggiornato)
+        st.success("Terapia salvata!")
