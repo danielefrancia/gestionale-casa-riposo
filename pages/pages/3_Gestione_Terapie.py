75458d54633f0baf8666080ebe62c4688581e861
danielefrancia <danielefrancia@gmail.com>
2026-07-03 16:00:23 +0200

    Update therapy management interface and functionality

--- pages/pages/3_Gestione_Terapie.py
+++ pages/pages/3_Gestione_Terapie.py
@@ -2,38 +2,22 @@ import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
-st.title("📋 Gestione Terapie")
+st.title("📋 Gestione e Stato Terapie")
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Leggiamo i dati di ospiti e terapie
-df_terapie = conn.read(worksheet="Terapie", ttl=0)
-df_ospiti = conn.read(worksheet="Ospiti", ttl=0)
+df = conn.read(worksheet="Terapie", ttl=0)
 
-# Creiamo un menu per filtrare le terapie per ospite
-lista_ospiti = df_ospiti['Nome'] + " " + df_ospiti['Cognome'] + " (ID: " + df_ospiti['ID'].astype(str) + ")"
-ospite_filtro = st.selectbox("Filtra terapie per ospite", ["Tutti"] + list(lista_ospiti))
+# Selezione terapia da aggiornare
+idx_da_aggiornare = st.selectbox("Seleziona la terapia da gestire", df.index, format_func=lambda x: f"{df.loc[x, 'Farmaco']} - {df.loc[x, 'Orario']}")
 
-if ospite_filtro != "Tutti":
-    id_selezionato = ospite_filtro.split("(ID: ")[1].replace(")", "")
-    df_visualizzato = df_terapie[df_terapie['ID Ospite'].astype(str) == id_selezionato]
-else:
-    df_visualizzato = df_terapie
-
-st.dataframe(df_visualizzato, use_container_width=True)
-
-# Cancellazione Terapia
-st.divider()
-st.subheader("❌ Rimuovi Terapia")
-
-# Creiamo un identificativo unico per la riga da rimuovere (es: Indice o Farmaco+Orario)
-if not df_visualizzato.empty:
-    idx_da_rimuovere = st.selectbox("Seleziona la riga della terapia da eliminare", df_visualizzato.index)
-    
-    if st.button("Elimina questa terapia"):
-        # Rimuoviamo la riga dal dataframe originale
-        df_terapie = df_terapie.drop(idx_da_rimuovere)
-        conn.update(worksheet="Terapie", data=df_terapie)
-        st.warning("Terapia rimossa correttamente!")
+col1, col2 = st.columns(2)
+with col1:
+    nuovo_stato = st.selectbox("Cambia Stato", ["Da fare", "Somministrato"])
+with col2:
+    if st.button("Aggiorna Stato"):
+        df.loc[idx_da_aggiornare, 'Stato'] = nuovo_stato
+        conn.update(worksheet="Terapie", data=df)
+        st.success("Stato aggiornato!")
         st.rerun()
-else:
-    st.info("Nessuna terapia trovata per questo ospite.")
+
+st.dataframe(df, use_container_width=True)
